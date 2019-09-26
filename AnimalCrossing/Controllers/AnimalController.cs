e5ceb69f42190a443d762c7b214f63c94f7561b8
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-09-26 13:24:11 +0200

    initial

---
+++ AnimalCrossing/Controllers/AnimalController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AnimalCrossing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AnimalCrossing.Controllers
+{
+    public class AnimalController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public string Hello()
+        {
+            return "Well, hello there! We are learning .NET Core now...";
+        }
+
+        public IActionResult MyFirstView()
+        {
+            ViewBag.MyWifeSays = "Go buy groceries, clean up, make dinner";
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Cat c)
+        {
+            return View("Thanks");
+        }
+
+
+
+    }
+}
---
+++ AnimalCrossing/Models/Cat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace AnimalCrossing.Models
+{
+    public class Cat
+    {
+        // C# properties = java attribute and public get and set.
+        //private int JavaCatId;
+
+        //public int getJavaCatId()
+        //{
+        //    return this.JavaCatId;
+        //}
+        //public void setJavaCatId(int catId)
+        //{
+        //    this.JavaCatId = catId;
+        //}
+
+
+
+        public int CatId { get; set; }
+        public string Name { get; set; }
+
+        // Later, create 1-to-many relationship to Species table
+        //public string Species { get; set; }
+
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+
+        public string ProfilePicture { get; set; }
+        public string Description { get; set; }
+
+        // Ratings..Comments, Reviews
+        // 
+        
+        public Cat()
+        {
+        }
+
+
+    }
+
+    public enum Gender { Male, Female, Other };
+}
