4b24c26ec4f3002fb27691933006c361db97e8b2
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-10-24 11:16:38 +0200

    seed

--- AnimalCrossing/Models/DbInitializer.cs
+++ AnimalCrossing/Models/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using AnimalCrossing.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -15,8 +16,37 @@ namespace AnimalCrossing.Models
             {
 
                 // You add more code here to seed database.
-                // See: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-2.2#initialize-db-with-test-data
+                // See: https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mvc-app/working-with-sql?view=aspnetcore-3.0&tabs=visual-studio
+                if (!context.Species.Any())
+                {
+                    var species = new Species[] {
+                        new Species { Name="Maine Coon", Description="Very big cat..." },
+                        new Species { Name="Sphynx", Description="With the right spelling" },
+                    };
 
+                    context.Species.AddRange(species);
+                    //foreach(var spec in species)
+                    //{
+                    //    context.Species.Add(spec);
+                    //}
+
+                    context.SaveChanges();
+                }
+
+                if (!context.Cats.Any())
+                {
+                    var speciesType = context.Species.ToList();
+
+                    var cats = new Cat[]
+                    {
+                        new Cat{ Name="Missy", Description="Likes walks on the beach", Gender=Gender.Female, BirthDate= new DateTime(2000, 4, 3),  ProfilePicture="https://www.zooplus.dk/magasin/wp-content/uploads/2018/01/fotolia_138361424-768x658.jpg", SpeciesId= speciesType[0].SpeciesId },
+                        new Cat{ Name="Baldy", Description="Likes something ", Gender=Gender.Female, BirthDate= new DateTime(2004, 4, 3),  ProfilePicture="https://images2.minutemediacdn.com/image/upload/c_fill,g_auto,h_1248,w_2220/f_auto,q_auto,w_1100/v1555382975/shape/mentalfloss/sphinxhed.png", SpeciesId=speciesType[1].SpeciesId },
+                    };
+                    context.Cats.AddRange(cats);
+
+                    
+                    context.SaveChanges();
+                }
 
 
             }
