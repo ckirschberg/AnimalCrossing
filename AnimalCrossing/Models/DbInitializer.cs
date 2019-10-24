c04d44ebfd158182f592301eab8b556e6f827091
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-10-24 10:02:04 +0200

    seed

---
+++ AnimalCrossing/Models/DbInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using AnimalCrossing.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AnimalCrossing.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AnimalCrossingContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<AnimalCrossingContext>>()))
+            {
+
+                // You add more code here to seed database.
+                // See: https://docs.microsoft.com/en-us/aspnet/core/data/ef-mvc/intro?view=aspnetcore-2.2#initialize-db-with-test-data
+
+
+
+            }
+
+
+        }
+    }
+}
--- AnimalCrossing/Program.cs
+++ AnimalCrossing/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AnimalCrossing.Data;
+using AnimalCrossing.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AnimalCrossing
 {
@@ -13,7 +16,24 @@ namespace AnimalCrossing
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+                try
+                {
+                    DbInitializer.Initialize(services);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred seeding the DB.");
+                }
+            }
+
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
